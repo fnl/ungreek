d4dc2a3467f832d988dc1d579629e7dba9c29e96
fnl <florian.leitner@gmail.com>
2014-01-21 10:35:36 +0100

    Py2 and Py3 compatibility

--- normal.py
+++ normal.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+from __future__ import print_function  # script works in Py2 and 3
 
 import unidecode  # pip install unidecode
 import fileinput
+import sys
+
+# unicode/str issues between Py2 and 3
+if sys.version < '3':
+    import codecs
+    def u(x):
+        return codecs.unicode_escape_decode(x)[0]
+else:
+    def u(x):
+        return x
 
 for line in fileinput.input():
-    print(unidecode.unidecode(line), end='')
+    print(unidecode.unidecode(u(line)), end='')
